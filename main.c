f98cefdc4e5bc9d5adc64e1eaf8d80080a6c4e40
diogomarchi <42180006+diogomarchi@users.noreply.github.com>
2020-05-04 20:19:24 -0300

    melhorei a escrita no main

--- main.c
+++ main.c
@@ -8,10 +8,11 @@
  * Created on 1 de Maio de 2020, 08:57
  */
 
-
 #include <xc.h>
 #include "config.h"
 #include "lcd_hd_44780.h"
+#include <stdio.h>
+#include <string.h>
 
 t_display_port *lcd;
 
@@ -24,42 +25,21 @@ void main(void) {
     function_set(lcd, 0, 1, 0);
     display_onoff_control(lcd, 1, 1, 0);
     entry_mode_set(lcd, 1,0);
-    goto_XY(lcd, 1, 1);
-        
-    write_char(lcd, 'D');
-    write_char(lcd, 'I');
-    write_char(lcd, 'O');
-    write_char(lcd, 'G');
-    write_char(lcd, 'O');
-    write_char(lcd, ' ');
-    write_char(lcd, 'M');
-    write_char(lcd, 'A');
-    write_char(lcd, 'R');
-    write_char(lcd, 'C');
-    write_char(lcd, 'H');
-    write_char(lcd, 'I');
+    goto_XY(lcd, 1, 1);                 //linha 1, coluna 1
     
-    goto_XY(lcd, 2, 4);
-    write_char(lcd, 'G');
-    write_char(lcd, 'E');
-    write_char(lcd, 'O');
-    write_char(lcd, 'R');
-    write_char(lcd, 'G');
-    write_char(lcd, 'E');
-    write_char(lcd, ' ');
-    write_char(lcd, 'D');
-    write_char(lcd, 'E');
-    write_char(lcd, ' ');
-    write_char(lcd, 'B');
-    write_char(lcd, 'O');
-    write_char(lcd, 'R');
-    write_char(lcd, 'B');
-    write_char(lcd, 'A');
+    char palavra[16] = "DIOGO MARCHI";
+    int tamanho = (int)strlen(palavra); //isto funciona só para delimitador de 1 caractere
+    for(int i = 0; i < tamanho; i ++)
+        write_char(lcd, palavra[i]);
+   
     
+    goto_XY(lcd, 2, 1);                 //linha 2, coluna 1
     
+    char palavra2[16] = "GEORGE DE BORBA";
+    int tamanho2 = (int)strlen(palavra2); //pega quantidade de caracteres
+    for(int i = 0; i < tamanho2; i ++)
+        write_char(lcd, palavra2[i]);
     
-    
-
         
     while(1){
         
